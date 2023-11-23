e3ae75e71d7d03c750c97a6e88368e262e858ed3
Kompudahtor <Kompudahtor@KOMPUDAHTOR>
2023-11-24 00:24:06 +0300

    6 laba

---
+++ DanilovA.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using DanilovA.Models;
+
+namespace DanilovA.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void IsValidMail_True()
+        {
+            var MailTest = new Prepod
+            {
+                Mail = "mm@mail.ru"
+            };
+
+            var result = MailTest.IsValidMail();
+
+            Assert.False(result);
+
+        }
+    }
+}
